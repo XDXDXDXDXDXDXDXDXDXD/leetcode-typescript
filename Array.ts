9c354b87dd7b75e5df352de46b653a04ba8b5064
XDXDXDXDXDXDXDXDXDXD <459058263@qq.com>
2021-03-01 21:01:55 +0800

    数组类--删除排序数组的重复值1和2（双指针）

--- Array.ts
+++ Array.ts
@@ -35,3 +35,51 @@ function moveZeroes(nums: number[]): void {
     right++
   }
 }
+
+/**
+ * 删除排序数组中的重复值
+ * @param nums
+ * @return 删除后数组的新长度
+ */
+function removeDuplicates(nums: number[]): number {
+
+  let i = 0
+  for (let j = 1; j < nums.length; ++j) {
+    let item = nums[j]
+    if (nums[i] !== item) {
+      nums[++i] = item
+    }
+  }
+  return i + 1
+
+  /*方法2 splice*/
+  // for (let i = 1; i < nums.length; ++i) {
+  //   if(nums[i] === nums[i - 1]) {
+  //     nums.splice(i, 1)
+  //     i--
+  //   }
+  // }
+  // return nums.length
+}
+
+/**
+ * 删除排序数组中的重复值2，每个元素最多出现2次
+ * @param nums
+ * @return 删除后数组的新长度
+ */
+function removeDuplicates2(nums: number[]): number {
+
+  let i = 0, flag = 0
+  for (let j = 1; j < nums.length; ++j) {
+    if (nums[j] === nums[i] && !flag) {
+      nums[++i] = nums[j]
+      flag++
+    } else if(nums[j] === nums[i] && flag) {
+      flag++
+    } else {
+      nums[++i] = nums[j]
+      flag = 0
+    }
+  }
+  return i + 1
+}
